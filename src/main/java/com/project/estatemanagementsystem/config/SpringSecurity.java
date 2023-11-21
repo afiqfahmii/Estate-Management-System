f464e366dfa01ef548a2befbba61cbbb1118e9ec
Afiq Fahmi <afqifahmi9858@gmail.com>
2023-11-21 15:16:01 +0800

    update

--- src/main/java/com/project/estatemanagementsystem/config/SpringSecurity.java
+++ src/main/java/com/project/estatemanagementsystem/config/SpringSecurity.java
@@ -30,7 +30,7 @@ public class SpringSecurity {
                 .authorizeHttpRequests((authorize) ->
                         authorize.requestMatchers("/register/**").permitAll()
                                 .requestMatchers("/index").permitAll()
-                                .requestMatchers("/users").hasRole("ADMIN")
+                                .requestMatchers("/users").hasRole("PEWARIS")
                 ).formLogin(
                         form -> form
                                 .loginPage("/login")
